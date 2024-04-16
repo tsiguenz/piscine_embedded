4642edfb973119de509fbc13ec7810b56da3a0aa
tsiguenz <tsiguenz@student.42.fr>
2024-04-16 13:35:18 +0200

    add day01 ex00

---
+++ day01/ex00/src/main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+  // set PB0 as output
+  DDRB |= (1 << PB1);
+  while (42) {
+    // wait 500ms 
+    for (uint64_t i = 0; i < 130000; i++) {}
+    // switch the light on PB0
+    PORTB ^= (1 << PB1);
+  }
+  return 0;
+}
