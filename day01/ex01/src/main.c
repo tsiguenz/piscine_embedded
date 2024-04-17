a79ed28c3db6809eb79843e1f62eef8e38e59578
tsiguenz <tsiguenz@student.42.fr>
2024-04-17 15:26:17 +0200

    fix tab instead of spaces and fix ex01

--- day01/ex01/src/main.c
+++ day01/ex01/src/main.c
@@ -8,12 +8,11 @@ int main(void) {
   TCCR1A |= (1 << WGM11 | 1 << WGM10);
   TCCR1B |= (1 << WGM13 | 1 << WGM12);
   // set top value
-  ICR1 = (F_CPU / 1024) / 2;
+  OCR1A = (F_CPU / 1024) / 2;
   // set switch led when timer is equal to OCR1A
   TCCR1A |= (1 << COM1A0);
   // set timer clock predivider to 1024
   TCCR1B |= (1 << CS12 | 1 << CS10);
-  while (1) {
-  }
+  while (1) {}
   return 0;
 }
--- day01/ex03/src/main.c
+++ day01/ex03/src/main.c
@@ -2,38 +2,38 @@
 #include <util/delay.h>
 
 int main(void) {
-	char cycle = 5;
-	// set buttons as input
-	DDRD &= ~(1 << PD2 | 1 << PD4);
-	// set PB1 as output
-	DDRB |= (1 << PB1);
-	// set mode 14 top is ICR1
-	TCCR1A |= (1 << WGM11);
-	TCCR1B |= (1 << WGM13 | 1 << WGM12);
-	// set top value
-	ICR1 = F_CPU / 1024;
-	// set switch led when timer is equal to OCR1A or ICR1
-	TCCR1A |= (1 << COM1A1);
-	// set timer clock predivider to 1024
-	TCCR1B |= (1 << CS12 | 1 << CS10);
-	while (1) {
-		// set value cycle percent of ICR1
-		OCR1A = (ICR1 / 10) * cycle;
-		// when button1 is pressed
-		if (!(PIND & (1 << PD2))) {
-			if (cycle < 10)
-				cycle += 1;
-			while (!(PIND & (1 << PD2)))
-				_delay_ms(50);
-		}
-		// when button2 is pressed
-		else if (!(PIND & (1 << PD4)) && cycle > 1) {
-			if (cycle > 1)
-				cycle -= 1;
-			while (!(PIND & (1 << PD2)))
-				_delay_ms(50);
-		}
-		_delay_ms(50);
-	}
-	return 0;
+  char cycle = 5;
+  // set buttons as input
+  DDRD &= ~(1 << PD2 | 1 << PD4);
+  // set PB1 as output
+  DDRB |= (1 << PB1);
+  // set mode 14 top is ICR1
+  TCCR1A |= (1 << WGM11);
+  TCCR1B |= (1 << WGM13 | 1 << WGM12);
+  // set top value
+  ICR1 = F_CPU / 1024;
+  // set switch led when timer is equal to OCR1A or ICR1
+  TCCR1A |= (1 << COM1A1);
+  // set timer clock predivider to 1024
+  TCCR1B |= (1 << CS12 | 1 << CS10);
+  while (1) {
+    // set value cycle percent of ICR1
+    OCR1A = (ICR1 / 10) * cycle;
+    // when button1 is pressed
+    if (!(PIND & (1 << PD2))) {
+      if (cycle < 10)
+        cycle += 1;
+      while (!(PIND & (1 << PD2)))
+        _delay_ms(50);
+    }
+    // when button2 is pressed
+    else if (!(PIND & (1 << PD4)) && cycle > 1) {
+      if (cycle > 1)
+        cycle -= 1;
+      while (!(PIND & (1 << PD2)))
+        _delay_ms(50);
+    }
+    _delay_ms(50);
+  }
+  return 0;
 }
