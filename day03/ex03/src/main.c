17e3ee57cbf23927d601be408e736d08bcafa6e7
tsiguenz <tsiguenz@student.42.fr>
2024-04-19 00:52:19 +0200

    fix lsp aleluia

--- day03/ex03/src/main.c
+++ day03/ex03/src/main.c
@@ -3,17 +3,17 @@
 
 #define BAUD_RATE 115200
 
-
 ISR(USART_RX_vect) {
   char c = UDR0;
   int switch_case = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-  (switch_case)? uart_tx(c ^ (1 << 5)) : uart_tx(c);
+  (switch_case) ? uart_tx(c ^ (1 << 5)) : uart_tx(c);
 }
 
 int main(void) {
   UCSR0B |= (1 << RXCIE0);
   sei();
   uart_init(GET_UBRR(BAUD_RATE));
-  while (1) {}
+  while (1) {
+  }
   return 0;
 }
