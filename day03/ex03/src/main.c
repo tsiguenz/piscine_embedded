17d563bc04983c7df1d8b5231c715c465b1781e7
tsiguenz <tsiguenz@student.42.fr>
2024-04-18 17:27:28 +0200

    add day03 ex03

---
+++ day03/ex03/src/main.c
@@ -0,0 +1,19 @@
+// http://www.rjhcoding.com/avrc-uart.php
+#include "uart.h"
+
+#define BAUD_RATE 115200
+
+
+ISR(USART_RX_vect) {
+  char c = uart_rx();
+  int switch_case = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+  (switch_case)? uart_tx(c ^ (1 << 5)) : uart_tx(c);
+}
+
+int main(void) {
+  UCSR0B |= (1 << RXCIE0);
+  sei();
+  uart_init(GET_UBRR(BAUD_RATE));
+  while (1) {}
+  return 0;
+}
