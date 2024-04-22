b5f58f45ba0c080f468792257308881f714c19b4
tsiguenz <tsiguenz@student.42.fr>
2024-04-22 17:54:05 +0200

    add day05 ex01

---
+++ day05/ex01/include/aht20.h
@@ -0,0 +1,11 @@
+#include "../include/i2c.h"
+#include "../include/uart.h"
+
+#define AHT20_ADDR 0x38
+#define AHT20_STATUS 0x71
+#define AHT20_CALIBRATE 0xBE
+#define AHT20_MESURE 0xAC
+
+void  aht20_init(void);
+void  aht20_mesure(float *humidity, float *temperature);
+
